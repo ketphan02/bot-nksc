ea11bf72278398dead984bfc316aa19dda454fe4
ketphan02 <tuankietvn@outlook.com>
2020-07-25 22:55:43 +0700

    update

--- events/post.mjs
+++ events/post.mjs
@@ -2,8 +2,28 @@ export default postEvent;
 
 function postEvent(app)
 {
-    app.post('/', (req, res) =>
-    {
-        console.log("POST");
-    });
+    const body = req.body;
+
+        if (body.object === 'page')
+        {
+
+            if (body.entry && body.entry.length <= 0) return;
+
+            /*----GET MESSAGE AND PRINT TO CONSOLE----*/
+            body.entry.forEach((entry) =>
+            {
+                entry.messaging.forEach((event) =>
+                {
+                    const sender_id = event.sender.id;
+                    
+                    if (event.message)
+                    {
+                        console.log(sender_id, event);
+                    }
+                });
+            });
+
+            res.status(200).send('RECIEVED');
+        }
+        else res.sendStatus(404);
 }
