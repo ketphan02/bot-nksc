54bcd6a3e2a05a2224304b952e0f621c22cd6ad8
ketphan02 <tuankietvn@outlook.com>
2020-07-25 22:16:25 +0700

    update

--- webhook/webhook.mjs
+++ webhook/webhook.mjs
@@ -23,5 +23,20 @@ function webhook()
 
     postEvent(app);
 
-    getEvent(app);
+    app.get('/', (req, res) =>
+    {
+        const mode = req.query['hub.mode'];
+        const token = req.query['hub.verify_token'];
+        const challenge = req.query['hub.challenge'];
+
+        if (mode && token)
+        {
+            if (mode === 'subscribe' && token === VERIFY_TOKEN)
+            {
+                console.log('VERIFIED');
+                res.status(200).send(challenge);
+            }
+            else res.sendStatus(403);
+        }
+    });
 }
