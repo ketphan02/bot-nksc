7cb1776a3fbf63346c60a31926392fd0f92064ab
ketphan02 <tuankietvn@outlook.com>
2020-07-25 22:52:00 +0700

    update

--- events/get.mjs
+++ events/get.mjs
@@ -7,7 +7,7 @@ const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
 function getEvent(app)
 {
-    app.get('/webhook', (req, res) =>
+    app.get('/', (req, res) =>
     {
         const mode = req.query['hub.mode'];
         const token = req.query['hub.verify_token'];
