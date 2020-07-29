74e3fa10ef5c40708ba48909e2e51702c91aa53b
ketphan02 <tuankietvn@outlook.com>
2020-07-30 01:03:40 +0700

    update

--- features/admin.mjs
+++ features/admin.mjs
@@ -70,7 +70,7 @@ async function fetchURL(link)
     };
 
     fetch(link, opt)
-    .then((res) => console.log(res.headers.values.toString))
+    .then((res) => console.log(res.headers))
     .catch((err) => console.log(err));
 }
 
