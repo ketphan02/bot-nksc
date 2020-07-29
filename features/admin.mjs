3cc40f9963c20d6dd4041e22d2a9447472aa1dda
ketphan02 <tuankietvn@outlook.com>
2020-07-29 22:56:21 +0700

    update

--- features/admin.mjs
+++ features/admin.mjs
@@ -40,7 +40,7 @@ async function curlURL(link, sender_id)
         request.get(link, (err, res, body) =>
         {
             global.isInit = false;
-            const first = '<div id="i1"';
+            const first = '<div id="i1" class="freebirdFormviewerComponentsQuestionBaseTitle exportItemTitle freebirdCustomFont" role="heading" aria-level="3">';
             const second = '</div><div class="freebirdFormviewerComponentsQuestionBaseDescription"';
 
             console.log(body.match(new RegExp(first + "(.*)" + second))[1]);
